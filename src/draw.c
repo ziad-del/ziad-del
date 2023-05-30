257ef2e91ac451d25ee1aafcc50ad90121673225
ziadalouani <ziad722@hotmail.com>
2023-05-30 23:31:31 +0200

    add random food generation

--- src/draw.c
+++ src/draw.c
@@ -8,29 +8,45 @@
 #include "draw.h"
 
 
-int init_game(SDL_Window ** window , SDL_Renderer ** renderer  ){
-
-
+int init_game(SDL_Window ** window, SDL_Renderer ** renderer){
   *window = SDL_CreateWindow("SNAKE", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
   if (*window == NULL) {
     fprintf(stderr, "SDL window failed to initialise: %s\n", SDL_GetError());
     return 1;
   }
-  
   *renderer = SDL_CreateRenderer(*window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-  
   if (*renderer == NULL) {
     fprintf(stderr, "SDL renderer failed to initialise: %s\n", SDL_GetError());
     return 1;
   }
-
+  draw_random_food_on_table(renderer);
 return 0;
 }
 
+void draw_random_food_on_table(SDL_Renderer ** renderer){
+SDL_Rect box;
+int random_number_WIDTH = rand() % WIDTH*0.95  + 10;
+int random_number_HEIGHT = rand() % HEIGHT*0.95 + 10;
+
+box.w = 15; 
+box.h = 15; 
+box.x = random_number_WIDTH;
+box.y = random_number_HEIGHT; 
+SDL_SetRenderDrawColor(*renderer, 255 , 0 , 0, 255);
+SDL_RenderDrawRect(*renderer, &box);
+SDL_RenderFillRect(*renderer, &box);
+}
+
+int draw_game(SDL_Renderer ** renderer){
+  SDL_SetRenderDrawColor(*renderer, 0, 0, 0, 255);
+  SDL_RenderClear(*renderer);
+  draw_random_food_on_table(renderer);
+  SDL_RenderPresent(*renderer);
+  return 0;
+}
 
 
-
-void destroy_game(SDL_Window **window,SDL_Renderer ** renderer ) {
+void destroy_game(SDL_Window **window,SDL_Renderer ** renderer) {
     SDL_DestroyRenderer(*renderer);
     SDL_DestroyWindow(*window);
     SDL_Quit();
--- src/logic.c
+++ src/logic.c
@@ -5,7 +5,7 @@
 #else
 #include <SDL2/SDL.h> /* macOS- and GNU/Linux-specific */
 #endif
-
+#include "draw.h"
 int run_game_logic( SDL_Renderer** renderer ){
     int quit = 0;
     // Event handler
@@ -13,15 +13,16 @@ int run_game_logic( SDL_Renderer** renderer ){
     // While application is running
     while (!quit) {
         // Handle events on queue
+        SDL_Delay(500);
+        draw_game(renderer);
         while (SDL_PollEvent(&event) != 0) {
             // Quit event (e.g., window close button)
             if (event.type == SDL_QUIT) {
                 quit = 1;
             }
         }
-        // Clear the renderer
-        SDL_RenderClear(*renderer);
-        SDL_RenderPresent(*renderer);
+        // SDL_RenderClear(*renderer);
+        // SDL_RenderPresent(*renderer);
     }
    return 0;
 }
